0928e70e4243c08cbd59d6d9d5fe796a0c98b94c
D7703_19 <d7703_19@d7703-19ui-imac.local>
2019-04-16 16:14:06 +0900

    시간데이터(DatePicker

--- DatePicker/ViewController.swift
+++ DatePicker/ViewController.swift
@@ -9,12 +9,26 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var lblTime: UILabel!
+    
+    @IBOutlet weak var datepic: UIDatePicker!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        // Do any additional setup after loadin정 the view, typically from a nib.// UI의 지역화(locatization)
+        let locale = Locale(identifier: "ko_KO")
+        datepic.locale = locale
+        
+        // DatePicker 모드 설정
+        datepic.datePickerMode = UIDatePicker.Mode.time
     }
 
-
+    @IBAction func changedate(_ sender: Any) {
+        let selectData = datepic.date
+        
+        let formatter = DateFormatter()
+        formatter.dateFormat = "a HH:mm "
+        lblTime.text = formatter.string(from: selectData)
+    }
+    
 }
 
